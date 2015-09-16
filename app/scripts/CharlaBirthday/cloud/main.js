94a894cdb63952bf0b66bb7cffdd17bf8660a7da
Chareesa <chareesagraham@gmail.com>
2015-09-15 19:02:26 -0700

    testing email rsvp to me

--- app/scripts/CharlaBirthday/cloud/main.js
+++ app/scripts/CharlaBirthday/cloud/main.js
@@ -13,25 +13,25 @@ Parse.Cloud.define("sendEmail", function(request, response) {
   var jan9 = request.params.jan9;
 
   sendgrid.sendEmail({
-   to: "chareesagraham@gmail.com",
-   from: email,
-   fromname: name,
-   subject: "This is for me, Chareesa",
-   text: name + " has rsvp'd"
-   },
+    to: "chareesagraham@gmail.com",
+    from: email,
+    fromname: name,
+    subject: "This is for me, Chareesa",
+    text: name + " has rsvp'd! <br> They will bring " + dish + " with them. <br> Will they stay the night? " + staythenight + ". Available dec 26? " + dec26 + ". Jan 1st? " + jan1 + ". Jan 9? " + jan9 + "."
+  },
 
    {
      success: function(httpResponse) {
        console.log(httpResponse);
        response.success("Email sent!");
-       console.log('success from main.js');
-    },
+       console.log("success from main.js");
+     },
      error: function(httpResponse) {
        console.error(httpResponse);
        response.error("Uh oh, something went wrong");
-       console.log('error from main.js');
-    }
-  });
+       console.log("error from main.js");
+     }
+   });
 
   sendgrid.sendEmail({
    to: email,
